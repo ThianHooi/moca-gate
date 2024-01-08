4d195a3103e667fd703422c967ba38c418987a84
ooithianhooi <ooithianhooi@gmail.com>
2024-01-08 11:49:11 +0800

    feat: test claiming reward with delegated wallet

--- contracts/MocaNft.sol
+++ contracts/MocaNft.sol
@@ -7,12 +7,11 @@ import "@thirdweb-dev/contracts/lib/CurrencyTransferLib.sol";
 
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
+import "./delegate-registry/IDelegateRegistry.sol";
+
 contract MocaNft is ERC721Base, PermissionsEnumerable {
     using EnumerableSet for EnumerableSet.UintSet;
     using EnumerableSet for EnumerableSet.AddressSet;
-
-    /// @dev Mapping to store the staking time of each NFT
-    mapping(uint256 => uint256) public stakingTime;
 
     /// @dev Set to keep store of staked NFTs
     EnumerableSet.UintSet private stakedNFTs;
@@ -20,24 +19,31 @@ contract MocaNft is ERC721Base, PermissionsEnumerable {
     /// @dev List of accounts that have staked their NFTs.
     EnumerableSet.AddressSet private _stakers;
 
-    /// @dev Mapping from staked token-id to staker address.
-    mapping(uint256 => address) public stakerAddress;
+    /// @dev Mapping to store the staking time of each NFT
+    mapping(uint256 => uint256) public stakingTime;
 
-    // Field to store the stake duration for reward
+    /// @dev Mapping to store whether the reward of an NFT has been claimed
+    mapping(uint256 => bool) public rewardClaimed;
+
+    /// @dev Field to store the stake duration for reward
     uint256 public stakeDurationForReward;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    address public DELEGATE_REGISTRY;
 
     constructor(
         string memory _name,
         string memory _symbol,
         uint256 _stakeDurationForReward,
         address _royaltyRecipient,
-        uint128 _royaltyBps
+        uint128 _royaltyBps,
+        address _delegateRegistryAddress
     ) ERC721Base(msg.sender, _name, _symbol, _royaltyRecipient, _royaltyBps) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(MINTER_ROLE, msg.sender);
         stakeDurationForReward = _stakeDurationForReward;
+        DELEGATE_REGISTRY = _delegateRegistryAddress;
     }
 
     function stakeNFT(uint256 tokenId) external {
@@ -48,7 +54,6 @@ contract MocaNft is ERC721Base, PermissionsEnumerable {
 
         stakingTime[tokenId] = block.timestamp;
         stakedNFTs.add(tokenId);
-
         _stakers.add(msg.sender);
     }
 
@@ -60,15 +65,70 @@ contract MocaNft is ERC721Base, PermissionsEnumerable {
         return block.timestamp - stakingTime[tokenId];
     }
 
-    function hasStakedNFT(address _staker) external view returns (bool) {
-        return _stakers.contains(_staker);
+    function hasStakedNFT(
+        address _staker,
+        uint256 tokenId
+    ) external view returns (bool) {
+        return _stakers.contains(_staker) && stakedNFTs.contains(tokenId);
     }
 
-    function isEligibleForReward(uint256 tokenId) external view returns (bool) {
+    /**
+     * @dev Returns whether the given token is eligible for reward
+     * @param tokenId token ID
+     * @param _cold cold address of the staker; set to 0x00 if not delegating
+     */
+    function isEligibleForReward(
+        uint256 tokenId,
+        address _cold
+    ) external view returns (bool) {
         require(stakedNFTs.contains(tokenId), "Not staked");
+
+        if (_cold != address(0)) {
+            bool isDelegateValid = IDelegateRegistry(DELEGATE_REGISTRY)
+                .checkDelegateForERC721(
+                    msg.sender,
+                    _cold,
+                    address(this),
+                    tokenId,
+                    ""
+                );
+
+            require(isDelegateValid, "delegation does not exist");
+        }
 
         uint256 stakingDuration = block.timestamp - stakingTime[tokenId];
         return stakingDuration >= stakeDurationForReward;
+    }
+
+    function claimReward(
+        uint256 tokenId,
+        address _cold
+    ) external returns (bool) {
+        require(stakedNFTs.contains(tokenId), "Not staked");
+        require(!rewardClaimed[tokenId], "Reward already claimed");
+
+        if (_cold != address(0)) {
+            bool isDelegateValid = IDelegateRegistry(DELEGATE_REGISTRY)
+                .checkDelegateForERC721(
+                    msg.sender,
+                    _cold,
+                    address(this),
+                    tokenId,
+                    ""
+                );
+
+            require(isDelegateValid, "delegation does not exist");
+        }
+
+        uint256 stakingDuration = block.timestamp - stakingTime[tokenId];
+        bool isValidForReward = stakingDuration >= stakeDurationForReward;
+
+        if (isValidForReward) {
+            rewardClaimed[tokenId] = true;
+            return true;
+        }
+
+        return false;
     }
 
     function withdrawNFT(uint256 tokenId) external {
@@ -76,8 +136,8 @@ contract MocaNft is ERC721Base, PermissionsEnumerable {
 
         transferFrom(address(this), msg.sender, tokenId);
 
+        stakingTime[tokenId] = 0;
         stakedNFTs.remove(tokenId);
-
-        _stakers.remove(msg.sender);
+        _stakers.remove(ownerOf(tokenId));
     }
 }
--- test/MocaNft.ts
+++ test/MocaNft.ts
@@ -1,10 +1,21 @@
 import { loadFixture, time } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
+import { http, zeroAddress } from 'viem';
+import { hardhat } from 'viem/chains';
+import { DelegateV2 } from '@delegatexyz/sdk';
+
+const RPC_URL = 'http://127.0.0.1:8545/';
 
 describe('MocaNft', function () {
   async function deployMocaNFTFixture() {
     const [owner, otherAccount, otherAccount2] = await ethers.getSigners();
+
+    const DelegateRegistry = await ethers.getContractFactory(
+      'DelegateRegistry'
+    );
+    const delegateRegistry = await DelegateRegistry.deploy();
+    const delegateRegistryContractAddress = await delegateRegistry.getAddress();
 
     const MocaNft = await ethers.getContractFactory('MocaNft');
     const mocaNft = await MocaNft.deploy(
@@ -12,10 +23,11 @@ describe('MocaNft', function () {
       'MOCA',
       604800,
       owner.address,
-      1
+      1,
+      delegateRegistryContractAddress
     );
 
-    return { mocaNft, owner, otherAccount, otherAccount2 };
+    return { mocaNft, owner, otherAccount, otherAccount2, delegateRegistry };
   }
 
   describe('Minting', function () {
@@ -54,15 +66,19 @@ describe('MocaNft', function () {
 
   describe('Staking', function () {
     it('NFT owner should be able to stake NFT', async function () {
-      const { mocaNft, owner } = await loadFixture(deployMocaNFTFixture);
+      const { mocaNft, owner, otherAccount } = await loadFixture(
+        deployMocaNFTFixture
+      );
 
-      const mintTx = await mocaNft.mintTo(owner.address, '');
+      const mintTx = await mocaNft.mintTo(otherAccount.address, '');
       await mintTx.wait();
 
-      const stakeTx = await mocaNft.stakeNFT(0);
+      const stakeTx = await mocaNft.connect(otherAccount).stakeNFT(0);
       await stakeTx.wait();
 
-      const stakedNft = await mocaNft.hasStakedNFT(owner.address);
+      const stakedNft = await mocaNft
+        .connect(otherAccount)
+        .hasStakedNFT(otherAccount.address, 0);
 
       expect(stakedNft).to.be.equal(true);
     });
@@ -91,7 +107,10 @@ describe('MocaNft', function () {
       const timstampNow = await time.latest();
       await time.increaseTo(timstampNow + 3 * 24 * 60 * 60 + 1);
 
-      const isEligibleForReward = await mocaNft.isEligibleForReward(0);
+      const isEligibleForReward = await mocaNft.isEligibleForReward(
+        0,
+        zeroAddress
+      );
 
       expect(isEligibleForReward).to.be.equal(false);
     });
@@ -109,9 +128,232 @@ describe('MocaNft', function () {
       const timstampNow = await time.latest();
       await time.increaseTo(timstampNow + 7 * 24 * 60 * 60 + 1);
 
-      const isEligibleForReward = await mocaNft.isEligibleForReward(0);
+      const isEligibleForReward = await mocaNft.isEligibleForReward(
+        0,
+        zeroAddress
+      );
 
       expect(isEligibleForReward).to.be.equal(true);
+    });
+  });
+
+  describe('Delegation of NFT', () => {
+    it('NFT owner should be able to delegate NFT', async function () {
+      const { mocaNft, owner, otherAccount, delegateRegistry } =
+        await loadFixture(deployMocaNFTFixture);
+
+      const contractAddress = await mocaNft.getAddress();
+      const delegateRegistryAddress = await delegateRegistry.getAddress();
+
+      const mintTx = await mocaNft.mintTo(owner.address, '');
+      await mintTx.wait();
+
+      const v2 = new DelegateV2(
+        // @ts-ignore
+        http(RPC_URL),
+        hardhat,
+        owner
+      );
+
+      // @ts-ignore
+      v2.contractConfig.address = delegateRegistryAddress;
+
+      await v2.delegateERC721(
+        otherAccount.address as `0x${string}`,
+        contractAddress as `0x${string}`,
+        0,
+        undefined,
+        true
+      );
+
+      const isDelegateForToken = await v2.checkDelegateForERC721(
+        otherAccount.address as `0x${string}`,
+        owner.address as `0x${string}`,
+        contractAddress as `0x${string}`,
+        0
+      );
+
+      expect(isDelegateForToken).to.be.equal(true);
+    });
+
+    it('NFT owner should be able to prove ownership with delegated wallet', async function () {
+      const { mocaNft, owner, otherAccount, delegateRegistry } =
+        await loadFixture(deployMocaNFTFixture);
+
+      const contractAddress = await mocaNft.getAddress();
+      const delegateRegistryAddress = await delegateRegistry.getAddress();
+
+      const mintTx = await mocaNft.mintTo(owner.address, '');
+      await mintTx.wait();
+
+      const stakeTx = await mocaNft.stakeNFT(0);
+      await stakeTx.wait();
+
+      // increase the time in Hardhat Network to 7 days later
+      const timstampNow = await time.latest();
+      await time.increaseTo(timstampNow + 7 * 24 * 60 * 60 + 1);
+
+      const v2 = new DelegateV2(
+        // @ts-ignore
+        http(RPC_URL),
+        hardhat,
+        owner
+      );
+
+      // @ts-ignore
+      v2.contractConfig.address = delegateRegistryAddress;
+
+      await v2.delegateERC721(
+        otherAccount.address as `0x${string}`,
+        contractAddress as `0x${string}`,
+        0,
+        undefined,
+        true
+      );
+
+      const isEligibleForReward = await mocaNft
+        .connect(otherAccount)
+        .isEligibleForReward(0, owner.address);
+
+      expect(isEligibleForReward).to.be.equal(true);
+    });
+
+    it('NFT owner should be able to claim reward with delegated wallet', async function () {
+      const { mocaNft, owner, otherAccount, delegateRegistry } =
+        await loadFixture(deployMocaNFTFixture);
+
+      const contractAddress = await mocaNft.getAddress();
+      const delegateRegistryAddress = await delegateRegistry.getAddress();
+
+      const mintTx = await mocaNft.mintTo(owner.address, '');
+      await mintTx.wait();
+
+      const stakeTx = await mocaNft.stakeNFT(0);
+      await stakeTx.wait();
+
+      // increase the time in Hardhat Network to 7 days later
+      const timstampNow = await time.latest();
+      await time.increaseTo(timstampNow + 7 * 24 * 60 * 60 + 1);
+
+      const v2 = new DelegateV2(
+        // @ts-ignore
+        http(RPC_URL),
+        hardhat,
+        owner
+      );
+
+      // @ts-ignore
+      v2.contractConfig.address = delegateRegistryAddress;
+
+      await v2.delegateERC721(
+        otherAccount.address as `0x${string}`,
+        contractAddress as `0x${string}`,
+        0,
+        undefined,
+        true
+      );
+
+      await expect(mocaNft.connect(otherAccount).claimReward(0, owner.address))
+        .not.to.be.reverted;
+    });
+
+    it('NFT owner should be not be able to claim reward again after claiming once', async function () {
+      const { mocaNft, owner, otherAccount, delegateRegistry } =
+        await loadFixture(deployMocaNFTFixture);
+
+      const contractAddress = await mocaNft.getAddress();
+      const delegateRegistryAddress = await delegateRegistry.getAddress();
+
+      const mintTx = await mocaNft.mintTo(owner.address, '');
+      await mintTx.wait();
+
+      const stakeTx = await mocaNft.stakeNFT(0);
+      await stakeTx.wait();
+
+      // increase the time in Hardhat Network to 7 days later
+      const timstampNow = await time.latest();
+      await time.increaseTo(timstampNow + 7 * 24 * 60 * 60 + 1);
+
+      const v2 = new DelegateV2(
+        // @ts-ignore
+        http(RPC_URL),
+        hardhat,
+        owner
+      );
+
+      // @ts-ignore
+      v2.contractConfig.address = delegateRegistryAddress;
+
+      await v2.delegateERC721(
+        otherAccount.address as `0x${string}`,
+        contractAddress as `0x${string}`,
+        0,
+        undefined,
+        true
+      );
+
+      const claimRewardTx = await mocaNft
+        .connect(otherAccount)
+        .claimReward(0, owner.address);
+      await claimRewardTx.wait();
+
+      await expect(mocaNft.connect(otherAccount).claimReward(0, owner.address))
+        .to.be.reverted;
+    });
+    
+    it('NFT owner should be not be able to claim reward with multiple delegated wallet', async function () {
+      const { mocaNft, owner, otherAccount, delegateRegistry, otherAccount2 } =
+        await loadFixture(deployMocaNFTFixture);
+
+      const contractAddress = await mocaNft.getAddress();
+      const delegateRegistryAddress = await delegateRegistry.getAddress();
+
+      const mintTx = await mocaNft.mintTo(owner.address, '');
+      await mintTx.wait();
+
+      const stakeTx = await mocaNft.stakeNFT(0);
+      await stakeTx.wait();
+
+      // increase the time in Hardhat Network to 7 days later
+      const timstampNow = await time.latest();
+      await time.increaseTo(timstampNow + 7 * 24 * 60 * 60 + 1);
+
+      const v2 = new DelegateV2(
+        // @ts-ignore
+        http(RPC_URL),
+        hardhat,
+        owner
+      );
+
+      // @ts-ignore
+      v2.contractConfig.address = delegateRegistryAddress;
+
+      // delegate to otherAccount
+      await v2.delegateERC721(
+        otherAccount.address as `0x${string}`,
+        contractAddress as `0x${string}`,
+        0,
+        undefined,
+        true
+      );
+      
+      // delegate to otherAccount2
+      await v2.delegateERC721(
+        otherAccount2.address as `0x${string}`,
+        contractAddress as `0x${string}`,
+        0,
+        undefined,
+        true
+      );
+
+      const claimRewardTx = await mocaNft
+        .connect(otherAccount)
+        .claimReward(0, owner.address);
+      await claimRewardTx.wait();
+
+      // otherAccount2 should not be able to claim reward, since otherAccount has already claimed on behalf of owner
+      await expect(mocaNft.connect(otherAccount2).claimReward(0, owner.address))
+        .to.be.reverted;
     });
   });
 });
